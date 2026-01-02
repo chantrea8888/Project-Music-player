2fa112c0e2dbff6e6035dc60afd6d3c7a4089ba8
Serey002 <serey.phem.12022006@gmail.com>
2026-01-02 14:23:40 +0700

    Function create playlist

--- JS/main.js
+++ JS/main.js
@@ -1421,6 +1421,7 @@ document.addEventListener('DOMContentLoaded', function () {
             elements.sidebarPlaylists.appendChild(playlistItem);
         });
     }
+    
     // Create a new playlist
     function createNewPlaylist() {
         Swal.fire({
